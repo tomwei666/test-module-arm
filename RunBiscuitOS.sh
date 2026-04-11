97144eef9cbfc303de77bb57d6e33188cfa05e3c
tom <tom_wei_666@163.com>
2026-04-11 20:22:29 +0800

    update make rootfs shell!

--- RunBiscuitOS.sh
+++ RunBiscuitOS.sh
@@ -12,7 +12,7 @@ RAM_SIZE=1024
 CMDLINE="earlycon root=/dev/vda rw rootfstype=ext4 console=ttyAMA0 init=/linuxrc loglevel=8"
 
 #QEMUT=/work/github/qemu/build/qemu-system-aarch64
-ROOT_1=/work/explore
+ROOT_1=/work/explore/126
 #KERNEL_IMG=/work/explore/out_k414/arch/arm64/boot/Image
 KERNEL_IMG=/work/explore/out_a_k510/arch/arm64/boot/Image
 
@@ -55,7 +55,23 @@ do_package()
 		echo 1
 	elif [ "$UBUNTU_VER" -eq 2 ]; then
 		echo "make rootfs under ubuntu2204"
-		#dd if=/dev/zero of=Freeze.img bs=1M count=512
+
+		# first,need to excute below
+		#mkdir -p rootfs_2204/dev/
+		#sudo mknod rootfs_2204/dev/tty1 c 4 1
+		#sudo mknod rootfs_2204/dev/tty2 c 4 2
+		#sudo mknod rootfs_2204/dev/tty3 c 4 3
+		#sudo mknod rootfs_2204/dev/tty4 c 4 4
+		#sudo mknod rootfs_2204/dev/console c 5 1
+		#sudo mknod rootfs_2204/dev/null c 1 3
+		#mkdir -p rootfs_2204/proc/
+		#mkdir -p rootfs_2204/sys/
+		#mkdir -p rootfs_2204/tmp/
+		#mkdir -p rootfs_2204/root/
+		#mkdir -p rootfs_2204/var/
+		#mkdir -p rootfs_2204/mnt/
+
+		dd if=/dev/zero of=Freeze.img bs=1M count=512
 		dd if=/dev/zero of=ramdisk bs=1M count=256
 		mkfs.ext4 -F ramdisk
 		rm -fr tmpfs
